fbf1f07e9af36f0736d23a089ae4cbb4321dc683
nika <puhohopla@yandex.ru>
2023-10-22 16:42:36 +0300

    DTO

--- my_app/ViewController.swift
+++ my_app/ViewController.swift
@@ -13,8 +13,37 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPurple
+        
+        let url = URL(string: "https://www.freetogame.com/api/games?platform=pc" )! // ура плохой способ (может вернуть nil, но нам пофиг)
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard // проверка на nil
+                let data, // let data = data
+//                let response,
+                error == nil
+                else { //если есть что-то в error то выходим
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            // обработка данных
+            let model = try! decoder.decode([GameDTO].self, from: data)
+//            let str = String(data: data, encoding: .utf8)
+//            print("Полученные данные: \(str ?? "")")
+            print(model)
+        }).resume()
+        
     }
+    
+    struct GameDTO: Decodable {
+        let id: Int
+        let title: String
+        let short_description: String
+        let thumbnail: URL
 
-
+        enum CodingKeys: String, CodingKey {
+            case id, title, short_description, thumbnail = "image_url"
+        }
+    }
 }
+
 
